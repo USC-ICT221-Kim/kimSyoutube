c8a0e4da3356846a26f8e9c4c67277621c6c0c8d
USC-ICT221-Kim <37359096+USC-ICT221-Kim@users.noreply.github.com>
2019-07-01 09:19:00 +1000

    Fixed some bugs on Custom VideoPlayer
    Worked on Video Pasue and Volume change.

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,17 +1,33 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
-const playBtn = document.getElementById("jsPlayButton");
+const playButton = document.getElementById("jsPlayButton");
+const volumeButton = document.getElementById("jsVolumeButton");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playButton.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     videoPlayer.pause();
+    playButton.innerHTML = '<i class="fas fa-play"></i>';
+
+  }
+}
+
+function handleVolumClick(){
+  if(videoPlayer.muted){
+    videoPlayer.muted = false;
+    volumeButton.innerHTML = '<i class="fas fa-volume-up"></i>';
+  }
+  else {
+    videoPlayer.muted = true;
+    volumeButton.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
 }
 
 function init() {
-  playBtn.addEventListener("click", handlePlayClick);
+  playButton.addEventListener("click", handlePlayClick);
+  volumeButton.addEventListener("click", handleVolumClick);
 }
 
 if (videoContainer) {
